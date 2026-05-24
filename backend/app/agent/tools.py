596ca6ab9c12c431750f81319b589487f8e7b8f1
Shourya <shouryav2003@gmail.com>
2026-05-24 16:21:07 +0530

    udpated files

--- backend/app/agent/tools.py
+++ backend/app/agent/tools.py
@@ -1,11 +1,9 @@
-#Defining the tools using tavily for reasearching
-from langchain_community.tools import TavilySearchResults
-from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
 import os
 
-#Tavily tool - retrieves the top 5 search results with URLs and content
-tavily_search = TavilySearchResults(max_results = 5, tavily_api_key = os.getenv("TAVILY_API_KEY"),
-                                    include_answers = True, include_raw_content = False, )
+tavily_search = TavilySearch(
+    max_results=5,
+    tavily_api_key=os.getenv("TAVILY_API_KEY"),
+)
 
-#List of tools that are initialized
 tools = [tavily_search]
